46468d6675e98699bc9c459f4b893be8b714511c
Harshit Gupta <harshit.gupta2@incedoinc.com>
2023-01-25 12:52:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [hour, setHour] = useState("");
+  const [minute, setMinute] = useState("");
+  const [sec, setSecond] = useState("");
+  const maxLength = "2";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <span className="container__title">Countdown Timer</span>
+
+      <div className="container__labels">
+        <p className="container__labels--label">Hours</p>
+        <p className="container__labels--label">Minutes</p>
+        <p className="container__labels--label">Seconds</p>
+      </div>
+
+      <div className="container__inputs">
+        <input
+          type="number"
+          maxLength={2}
+          value={hour}
+          onInput={(event) => setHour(event.target.value.slice(0, maxLength))}
+          className="container__inputs--time hour"
+          placeholder="00"
+        />
+        <p className="container__inputs--colon">:</p>
+        <input
+          type="number"
+          maxLength={2}
+          value={minute}
+          onInput={(event) => setMinute(event.target.value.slice(0, maxLength))}
+          className="container__inputs--time minute"
+          placeholder="00"
+        />
+        <p className="container__inputs--colon">:</p>
+        <input
+          type="number"
+          maxLength={2}
+          value={sec}
+          onInput={(event) => setSecond(event.target.value.slice(0, maxLength))}
+          className="container__inputs--time sec"
+          placeholder="00"
+        />
+      </div>
+
+      <div className="container__btns">
+        <button className="btn start">Start</button>
+        <button className="btn stop">Stop</button>
+        <button className="btn reset">Reset</button>
+      </div>
     </div>
   );
 }
